954faf026692004b339e44887af098a39488bd35
Hristina Dokic <55371758+hdjokic@users.noreply.github.com>
2023-05-15 21:58:29 -0500

    package-info-added individual work info

--- src/main/java/com/group7/recipes/security/package-info.java
+++ src/main/java/com/group7/recipes/security/package-info.java
@@ -1,10 +1,18 @@
 /**
- * To enable relational database
- * <ol>
- * <li>WebSecurityConfig - uncomment the header
- * <li>UserDetailsServiceImple - uncomment the service
- * <li>RegistrationService - uncomment the service
- * <li>App - uncomment the Bean
- * </ol>
+ * Managed by Hristina Dokic
+ * 
+ * Created User and User Repo 
+ * Creater UserRole and UserRole repo
+ * added one to many relationship from user to userrole
+ * Created SignUp page to secure the application
+ * Created web security configuration
+ * Created code that implements the UserDetailsService interface of Spring Security in UserDetailsServicesImpl.
+ * Created java code that implements the UserDetails interface of Spring Security. The UserDetails interface is used to provide core user information. Check UserDetailImpl
+ * 
+ * @see User for user table constraints and columns
+ * @see UserRole for user role table and its constraints
+ * @see WebSecurityConfig for the security configuration
+ * @see UserRoleRoleType for the available roles to the user
+ * @author Hristina Dokic
  */
 package com.group7.recipes.security;
