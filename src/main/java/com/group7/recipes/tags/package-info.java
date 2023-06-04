153adab8ca43ce5c414dfe774eb7abae5ec41253
Craig Terry <crgtrr@gmail.com>
2023-06-03 21:43:21 -0500

    Added to tags package-info.java

--- src/main/java/com/group7/recipes/tags/package-info.java
+++ src/main/java/com/group7/recipes/tags/package-info.java
@@ -8,4 +8,6 @@ package com.group7.recipes.tags;
  * - @see Tags identifies the category for each type of recipe and ingredient
  * - @see RecipeTags ties/associates recipes to tags and tags to recipes
  * Designing initial html (index.html)
+ * Tags was designed to be more than id/tag name ... I spent the ramp up time configuring a 
+ *    tag hierarchy that would provide a very flexible utility.
  **/
