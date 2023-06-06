15c023200563b69dc27c67d838dcf6373f5d9f94
Craig Terry <crgtrr@gmail.com>
2023-06-05 19:01:43 -0500

    tags package info

--- src/main/java/com/group7/recipes/tags/package-info.java
+++ src/main/java/com/group7/recipes/tags/package-info.java
@@ -1,13 +1,22 @@
-package com.group7.recipes.tags;
 /**
- * Base Account Data for users
+ * Tag implementation
  * Managed by Craig Terry
  *
- * Created models: Recipe, Tags, RecipeTags, and User models
+ * Created/configured initial repository and group
+ * Created initial models: Recipe, Tags, RecipeTags, and User models
+ * Created code that implements tag operations providing filtering of recipes based on predefined items
  * Designed a unique tags implementation
- * - @see Tags identifies the category for each type of recipe and ingredient
- * - @see RecipeTags ties/associates recipes to tags and tags to recipes
+ * @see Tag identifies the category for each type of recipe and ingredient
+ * @see RecipeTag ties/associates recipes to tags and tags to recipes
+ * @see RecipeTagRepository for java interface that extends CrudRepository interfase
+ * @see TagRepository for java interface that extends CrudRepository interface
+ * @see TagService implements REST operations for tags
+ * @see RecipeTagService implements REST operations for RecipeTags
+ * @see TagTest unit testing tag operation
+ * @see TagServiceTest unit testing Tag REST services
  * Designing initial html (index.html)
  * Tags was designed to be more than id/tag name ... I spent the ramp up time configuring a 
  *    tag hierarchy that would provide a very flexible utility.
+ * @author Craig Terry
  **/
+package com.group7.recipes.tags;
